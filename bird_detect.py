0a0bb36c797be8e8a97345c42f3fb5f101d290c6
John Gerving <jhg51@humboldt.edu>
2024-09-16 18:18:57 +0000

    Added read_and_write_clips function for taking in clip information and writing from the original video to new output clips

--- bird_detect.py
+++ bird_detect.py
@@ -53,7 +53,6 @@ def process_video(gpu_num, file_name, start_index, stop_index):
             
         frame_number += 1
 
-    print(detections)
     return detections
         
 
@@ -67,6 +66,103 @@ def total_video_frames(file_name):
 
     cap = cv2.VideoCapture(file_name)
     return int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+
+def calculate_clip_bounds(batches, padding=90):
+    '''Calculates the start and end indices of the clips where detections occurred.
+
+    :param batches: A generator object, with each entry containing a NumPy boolean array, each entry corresponding to a frame and True representing a detection occurring.
+    :param padding: The number of frames before and after the detection to include as padding in the clips.
+
+    :return: A two-dimensional list, with each element having two elements representing the start and end frame index of a clip. 
+    '''
+
+    currently_reading_clip = False
+    frame_index = 0
+    frames_since_last_detection = 0
+
+    clips = []
+
+    # Loop through batches
+    for batch in batches:
+        # Loop through each frame
+        for frame in batch:
+            if frame == True:
+                # Reset frames since last detection if there was a detection in the frame
+                frames_since_last_detection = 0
+
+                # If a clip isn't already being read, add a new entry to the clips with the current frame index
+                if not currently_reading_clip:
+                    currently_reading_clip = True
+                    start = max(frame_index - padding, 0)
+                    clips.append([start])
+            else:
+                # Update frames since last detection if no detection in frame
+                frames_since_last_detection += 1
+                
+                # If there is a clip currently being read and the frames since the last detection exceeds the padding, end the current clip and reset variables
+                if currently_reading_clip and frames_since_last_detection > padding:
+                    end = frame_index
+
+                    clips[-1].append(end)
+
+                    currently_reading_clip = False
+                    frames_since_last_detection = 0
+
+            frame_index += 1
+
+    return clips
+
+def read_and_write_clips(file_name, clips, output_dir):
+    '''Reads a file from a path name and writes clips from it to new files.
+    
+    :param file_name: The path to the original file to read clips from.
+    :param clips: An array of clips with start and end indices to read from.
+    :param output_dir: The directory to write clips to.
+    '''
+
+    cap = cv2.VideoCapture(file_name)
+
+    # Check if opened successfully
+    if cap.isOpened() == False:
+        raise Exception("Error opening video file")
+
+    # Get width and height of frame
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_size = (frame_width, frame_height)
+    # Get FPS of video
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Loop through clips
+    for i in range(len(clips)):
+        # Write to MP4 format
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        # Create new video writer for clip
+        video = cv2.VideoWriter(f'{output_dir}/{i}.mp4', fourcc, fps, frame_size)
+
+        # Set the current read frame to the start index of the clip
+        cap.set(cv2.CAP_PROP_POS_FRAMES, clips[i][0] - 1)
+
+        # Set current frame to start index of the clip
+        current_frame = clips[i][0]
+
+        # Loop through the clip until it is closed or the end index of the clip is reached
+        while cap.isOpened() and current_frame <= clips[i][1]:
+            # Read each frame of video
+            ret, frame = cap.read()
+
+            if ret == True:
+                # Write the frame to the output file
+                video.write(frame)
+            else:
+                break
+
+            current_frame += 1
+
+        video.release()
+
+    cap.release()
+    
 
 def main():
     if GPU_COUNT < 1:
@@ -108,7 +204,10 @@ def main():
     num_batches = batch_num
         
     with ProcessPoolExecutor(max_workers=num_batches) as executor:
-        executor.map(process_video, devices, file_names, start_indices, end_indices)
+        frame_detections = executor.map(process_video, devices, file_names, start_indices, end_indices)
+
+        clips = calculate_clip_bounds(frame_detections)
+        read_and_write_clips(FILE_NAME, clips, "output")
 
 if __name__ == '__main__':
     main()
